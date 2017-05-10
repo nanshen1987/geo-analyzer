8f969d86cdcc780573715f27f4df21fcaab29fd7
nanshen1987 <sn_gis@163.com>
2017-05-10 23:28:23 +0800

    Initial commit

---
+++ src/main/java/com/sp/gis/Main4Dev.java
@@ -0,0 +1,54 @@
+package com.sp.gis;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by sn on 2017-5-10.
+ */
+@SpringBootApplication
+public class Main4Dev implements CommandLineRunner {
+    public static void main(String[] args) throws IOException {
+        // display a data store file chooser dialog for shapefiles
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            return;
+        }
+
+        FileDataStore store = FileDataStoreFinder.getDataStore(file);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        // Create a map content and add our shapefile to it
+        MapContent map = new MapContent();
+        map.setTitle("Quickstart");
+
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(featureSource, style);
+        map.addLayer(layer);
+
+        // Now display the map
+        JMapFrame.showMap(map);
+
+        SpringApplication.run(Main4Dev.class, args);
+
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
